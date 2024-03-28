3031021ba3479a36e9ffe6e43ad4f42d17faa5c4
cshipley98 <claireshipley98@gmail.com>
2024-03-27 20:02:30 -0500

    first error

--- routes/server.js
+++ routes/server.js
@@ -2,3 +2,8 @@ const inquirer = require('inquirer');
 const db = require('./db/connection');
 
 //start server after db connection
+db.connect(err => {
+    if (err) throw err;
+
+    promptUser();
+});
