7f4450142cb6cc2dff57b2ca90d476b48c64e4bc
cshipley98 <claireshipley98@gmail.com>
2024-03-27 20:34:09 -0500

    const prompt user

--- routes/server.js
+++ routes/server.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 
+const promptUser = () => {
+
+};
+
 //start server after db connection
 db.connect(err => {
     if (err) throw err;
