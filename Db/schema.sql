9ef1a1e491abad6521e8eb672e03b86023a6f478
cshipley98 <claireshipley98@gmail.com>
2024-04-02 09:38:25 -0500

    departments table

---
+++ Db/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
+);
