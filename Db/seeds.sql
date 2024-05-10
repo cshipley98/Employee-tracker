47143ad64ea828f61fd13876dbedee448fae38a4
cshipley98 <claireshipley98@gmail.com>
2024-05-10 10:04:27 -0500

    employee names

--- Db/seeds.sql
+++ Db/seeds.sql
@@ -18,3 +18,18 @@ VALUES
     ('Lawyer', 120000, 4),
     ('Sales Represenatitve', 60000, 2),
     ('Human Resources Represenatitve', 55000, 6);
+
+    INSERT INTO employees (first_name, last_name, role_id, manager_id)
+    VALUES
+    ('Claire', 'Kyle', 5, null),
+    ('Will', 'Smith', 1, 1),
+    ('Reid', 'Carter', 2 , 2),
+    ('Sean', 'King', 1, 1),
+    ('Fay', 'Yung', 2, 4),
+    ('Kimmi', 'lockhouse', 4, null),
+    ('Jamie', 'Haines', 7, 1),
+    ('Andrew', 'Craig', 8, null),
+    ('Jessica', 'Smith', 3, 3),
+    ('Brittany', 'Feltnerr', 6, null),
+    ('Rachel', 'Franks', 3, 5),
+    ('Natalie', 'Gurel', 4, null);
