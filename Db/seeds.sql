2bdf9bef17ea10aae6178ad3e0d3653207eef529
cshipley98 <claireshipley98@gmail.com>
2024-05-10 10:02:15 -0500

    insert into roles

--- Db/seeds.sql
+++ Db/seeds.sql
@@ -6,3 +6,15 @@ VALUES
     ('Legal'),
     ('Accounting'),
     ('Human Resources');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+
+('Lead Engineer', 140000, 3),
+    ('Software Engineer', 110000, 3),
+    ('Junior Developer', 70000, 3),
+    ('Acountant', 112000, 5),
+    ('Project Manager', 90000, 1),
+    ('Lawyer', 120000, 4),
+    ('Sales Represenatitve', 60000, 2),
+    ('Human Resources Represenatitve', 55000, 6);
