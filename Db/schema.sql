b58beabe3034ef6e7434b13cc84d7165c38fd5ff
cshipley98 <claireshipley98@gmail.com>
2024-05-10 09:55:54 -0500

    create table roles

--- Db/schema.sql
+++ Db/schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE departments (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
+    CREATE TABLE roles (
+        id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(30),
+        last_name VARCHAR(30),
+        role_id INTEGER,
+        manager_id INTEGER,
+        FOREIGN KEY (department_id) REFERENCES departments(id)
+    );
+
+    
