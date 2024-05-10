e2fe277c04f2e5e0338002dd3271e69fa2b4b642
cshipley98 <claireshipley98@gmail.com>
2024-05-10 09:11:17 -0500

    line 10 error resolved

--- Db/connection.js
+++ Db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: password,
-        database: 'employee_tracker' s
+        database: 'employee_tracker' 
     },
   
             console.log('Connected to the employee_trackrt databse.')
