15e8bbc453054f497f59de0bd61d3334e64df782
cshipley98 <claireshipley98@gmail.com>
2024-05-10 10:11:36 -0500

    selection prompt

--- routes/server.js
+++ routes/server.js
@@ -11,3 +11,27 @@ db.connect(err => {
 
     promptUser();
 });
+
+function promptUser() {
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+            'View All Employees',
+                'View All Employees by Department',
+                'View All Departments',
+                'View All Roles',
+                'View All Department Budgets',
+                'Update Employee Role',
+                'Update Employee Manager',
+                'Add Employee',
+                'Add Role',
+                'Add Department',
+                'Delete Employee',
+                'Delete Role',
+                'Delete Department',
+                'Quit'
+        ]
+    })
+}
