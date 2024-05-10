b9c81aa3477765bbf70d597e1a88afeb7a8a925e
cshipley98 <claireshipley98@gmail.com>
2024-05-10 09:58:42 -0500

    created tables-employees

--- Db/schema.sql
+++ Db/schema.sql
@@ -15,4 +15,12 @@ CREATE TABLE departments (
         FOREIGN KEY (department_id) REFERENCES departments(id)
     );
 
-    
+        CREATE TABLE employees (
+            id INTEGER NOT NULL AUTO_INCREMENTS PRIMARY KEY,
+            first_name VARCHAR(30),
+            last_name VARCHAR(30),
+            role_id INTEGER,
+            manager_id INTEGER, 
+            FOREIGN KEY (role_id) REFERENCES roles(id),
+            FOREIGN KEY (manager_id) REFERENCES employees(id)
+        );
